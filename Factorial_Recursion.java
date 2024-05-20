e0275b872c20407ada0e57fc4f1c5bd01e9c2b81
Shruti Sharma <53565103+ShrutiSharma-27@users.noreply.github.com>
2024-05-20 20:32:17 +0530

    Add files via upload

---
+++ Factorial_Recursion.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Factorial_Recursion {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter no : ");
+        int number = input.nextInt();
+        System.out.println(factorial(number));
+    }
+
+    public static int factorial(int n){
+        if (n==1) return 1;
+        else return n * factorial(n-1);
+    }
+}
