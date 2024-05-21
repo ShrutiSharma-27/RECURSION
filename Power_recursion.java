ef1f766bdb5d511633989aa338722eff5c8ff809
Shruti Sharma <53565103+ShrutiSharma-27@users.noreply.github.com>
2024-05-21 12:51:16 +0530

    Add files via upload

---
+++ Power_recursion.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Power_recursion {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter no : ");
+        int number = input.nextInt();
+        System.out.print("enter power : ");
+        int pow = input.nextInt();
+        System.out.println(power(number, pow));
+    }
+
+    public static int power(int x,int n){
+        if (n==0) return 1;
+        return x*power(x,n-1);
+    }
+}
