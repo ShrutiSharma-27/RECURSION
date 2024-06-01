2e97cde43e14807ff6c145b0553e4c340f1020a7
Shruti Sharma <53565103+ShrutiSharma-27@users.noreply.github.com>
2024-06-01 14:03:29 +0530

    Add files via upload

---
+++ All_indicesOfElement_recursion.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class All_indicesOfElement_recursion {
+    public static void main(String... args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter no of elements : ");
+        int n = input.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("enter element : ");
+            arr[i] = input.nextInt();
+        }
+
+        System.out.print("which element's indices you want : ");
+        int x = input.nextInt();
+
+        System.out.println(Arrays.toString(all_indices(arr,x,0,0)));
+    }
+
+    public static int[] all_indices(int[] arr, int x, int idx, int fsf){ //fsf - no of times x is "found so far"
+        if (idx == arr.length){
+            return new int[fsf];
+        }
+
+        int[] i_arr; //extracted common part
+        if (arr[idx] == x){
+            i_arr = all_indices(arr,x, idx + 1, fsf + 1);
+            i_arr[fsf] = idx;
+        }
+        else{
+            i_arr = all_indices(arr,x,idx + 1, fsf);
+        }
+        return i_arr; //extracted common part
+    }
+}
